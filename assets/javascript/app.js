49615e88f099d82e67e3ea39f77b60897356cc90
ykeanu <ykhernan@ucla.edu>
2017-09-18 19:57:39 -0700

    Added start button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,19 @@
 // i. Global Variables + Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var qa = [{
         question: "Who won NBA's Most Valuable Player during the 2016-2017 season?",
-        answers: ["Russell Westbrook", "Sunny", "Raining", "Windy"]
+        answers: ["Russell Westbrook", "James Harden", "Kevin Durant", "Lebron James"]
     },
     {
         question: "What NBA hoopster is known as the \"Baby-Faced Assasin\"?",
-        answers: ["Stephen Curry", "Answer 2.2", "Answer 2.3", "Answer 2.4"]
+        answers: ["Stephen Curry", "Kyrie Irving", "Lonzo Ball", "Devin Booker"]
     },
     {
         question: "Who was the last team to win 3 consecutive championships?",
-        answers: ["Los Angeles Lakers", "Answer 3.2", "Answer 3.3", "Answer 3.4"]
+        answers: ["Los Angeles Lakers", "Chicago Bulls", "Boston Celtics", "Golden State Warriors"]
     },
     {
-        question: "Which of the following dances was popularized by Houston Rocket star, James Harden?",
-        answers: ["Swag Cooking", "Dabbing", "Answer 4.3", "Answer 4.4"]
+        question: "Which of the following dances was popularized by star of the Houston Rockets, James Harden?",
+        answers: ["The Swag Cook", "The Dab", "The Quan", "Square Dancing"]
     },
     {
         question: "In pop culture, which basketball family is considered to be the \"Kardashians\" of the NBA?",
@@ -21,12 +21,10 @@ var qa = [{
     }
 ];
 
-var answerArr = ["Russell Westbrook", "Stephen Curry", "Los Angeles Lakers", "Swag Cooking", "Ball Family"];
+var answerArr = ["Russell Westbrook", "Stephen Curry", "Los Angeles Lakers", "The Swag Cook", "Ball Family"];
 // var oldQ[];
 // var arrayofAnswers[];
 var count = 0;
-
-
 
 // ii. Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //Fisher Yates Shuffle method (shuffles questions or answers when called)
@@ -45,58 +43,58 @@ Array.prototype.shuffle = function() {
 //Displays correct answers image
 function displayImage() {
     $('#image-holder').html("<img src='assets/images/" + imageArr[count] + ".png' width='400px'>");
-}
+};
 
 //Displays questions
 function displayQuestion(Q) {
-    // for (var i = 0; i < 5; i++) {
-    //Shuffle answers
-    // aShuffled = qaShuffled[0].answers.shuffle();
     $('.question').html("<h2>" + qaShuffled[Q].question + "</h2>");
     $('.answerA').html("<button>A: <span class='options'>" + aShuffled[0] + "</span></button>");
     $('.answerB').html("<button>B: <span class='options'>" + aShuffled[1] + "</span></button>");
     $('.answerC').html("<button>C: <span class='options'>" + aShuffled[2] + "</span></button>");
     $('.answerD').html("<button>D: <span class='options'>" + aShuffled[3] + "</span></button>");
-    // }
 };
-
 
 //Shuffle Questions
 var qaShuffled = qa.shuffle();
 //Shuffle Answers
 var aShuffled = qaShuffled[0].answers.shuffle();
 
-var test;
-
 
 // +++++++++
 // function stopWatch() {
 
-//     }
+// };
+
+function startGame (Q) {
+    displayQuestion(Q);
+    // setInterval(displayImage, 30000)
+};
+
 
 // iii. Main Process ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Display the page with the start button
 $(document).ready(function() {
 
-    //Click even that begins game
+    // Click on button to begin game
+    $('#start').on('click', function () {
+        startGame(count);
+    });
+    // displayQuestion(0);
 
-    // if (userinput == array [i])
+    // function startGame() {
+    //     setInterval(displayImage, 30000)
+    // }
 
-
-    displayQuestion(0);
-
-    function startGame() {
-        setInterval(displayImage)
-    }
-
-    // ++++++++++
+    // // ++++++++++
     // stopWatch();
 
-    $(".options").on('click', function() {
-        var userChoice = $(this).html();
-        console.log(userChoice, "Choice");
-        console.log(answerArr.indexOf(userChoice), "IndexOf");
-        if (answerArr.indexOf(userChoice) >= 0) {
+
+    // Determines whether answer is correct or wrong
+    $(document).on('click', '.options', function() {
+        var userSelect = $(this).html();
+        console.log(userSelect, "Choice");
+        console.log(answerArr.indexOf(userSelect), "IndexOf");
+        if (answerArr.indexOf(userSelect) >= 0) {
             alert("Correct!");
         } else {
             console.log("Try again.");
@@ -104,49 +102,12 @@ $(document).ready(function() {
 
 
 
-        // console.log($(this).html());
-        // test = $(this).html();
-        // console.log(test, "INSIDE")
-
     });
 
 
 
     //TESTING
     // console.log(test, "OUTSIDE");
-
-    // for (var i=0; i < 4; i++) {
-    // if (qa[i].question != oldQ)    
-    // function displayQ() {
-    //     $('.question').html("<h2>" + qa[0].question + "</h2>");
-    //     $('.answerA').html("<button>A: " + newArr[0] + "</button>");
-    //     $('.answerB').html("<button>B: " + newArr[1] + "</button>");
-    //     $('.answerC').html("<button>C: " + newArr[2] + "</button>");
-    //     $('.answerD').html("<button>D: " + newArr[3] + "</button>");
-    // };
-
-    // };
-
-
-
-
-    // Playing around with loop
-    // for (i = 0; i < qa[0].answers.length; i++ ){
-    //     $("#q1").append("<button class='test" + i + "'>" + qa[0].answers[i] + "</button>");
-    // }
-
-    // Shuffled array
-    // var newArr = qa[0].answers.shuffle();
-    // $("#q1").on('click', function(){
-    //     console.log(this);
-    // });
-
-    // TESTING
-    // console.log (qa[0].answers[1]);
-    // console.log(qa[0].answers[0]);
-    // console.log(newArr);
-    // console.log(newArr[0]);
-
 
 
 
